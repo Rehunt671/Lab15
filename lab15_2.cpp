92a2f25833b32f9091a79f9cd08fd0c66e4fd869
dfssfd <dfssfd>
2023-01-17 15:16:40 +0700

    Success

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 const int N = 5;
 
 void inputMatrix(double [][N]);
@@ -18,4 +17,30 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+void inputMatrix(double x[N][N]){
+  for (int i = 0 ; i < N ; i++){
+    cout << "Row " << i+1 << ": ";
+    for(int j = 0 ; j < N ; j++){
+      cin >> x[i][j];
+    }
+  }
+}
+void findLocalMax(const double x[N][N], bool y[N][N]){
+  for(int i = 0 ; i < N ; i++){
+    for(int j = 0 ; j < N ;j++){
+      if(i == 0 || i == N-1 || j == 0 || j == N-1) y[i][j] = 0;
+      else if(x[i][j] >= x[i][j-1] && x[i][j] >= x[i][j+1] && x[i][j] >= x[i-1][j] && x[i][j] >= x[i+1][j] ) y[i][j] = 1;
+      else y[i][j] = 0 ;
+    }
+  }
+}
+
+void showMatrix(const bool x[N][N]){
+  for(int i = 0 ; i < N ; i++ ){
+    for(int j = 0 ; j < N ;j++ ){
+      cout << x[i][j] << " ";
+    }
+    cout << "\n";
+  }
 }
